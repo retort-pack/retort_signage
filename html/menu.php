8357e46bff049d890d2d720926562e6c64b32817
takeshi0511 <ts03511@gmail.com>
2018-12-15 11:50:29 +0900

    menu.php base create

--- html/menu.php
+++ html/menu.php
@@ -4,5 +4,22 @@
 	
 </head>
 	<body>
+		<div id="master">
+			<table>
+<!-- ヘッダー -->
+				<tr>
+					<th>
+					</th>
+				</tr>
+<!-- ヘッダー -->
+
+<!-- 表のデータ。ループ回す。 -->
+				<tr>
+					<td>
+					</td>
+				</tr>
+<!-- 表のデータ -->
+			</table>
+		</div>
 	</body>
 </html>
