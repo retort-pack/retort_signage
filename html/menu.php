b1e9bfa4c757054405d1839b60df0d7357ed912d
takeshi0511 <ts03511@gmail.com>
2018-12-19 01:39:41 +0900

    php code written

--- html/menu.php
+++ html/menu.php
@@ -11,10 +11,9 @@
 	$db_user -> Database User.
 	$db_password -> Database User's Password.
 	$db_name -> Database's Name.
-	$menu_index -> DB Data Number List.
-	$menu_no -> Number of Menu list.
 	$menu_sql -> SQL.
 	$sql_result -> exec SQL Result.
+	$menu_info -> Selected menu information.
 -->
 
 	<body>
@@ -22,12 +21,12 @@
 
 <!-- Connect to MySQL/MariaDB -->
 <!-- This is TEST ONLY!!!!!! -->
-<?php
-	$db_host = '127.0.0.1';
-	$db_user = 'kingpelican_mgmt';
-	$db_password = 'kingpelican2016';
-	$db_name = 'kingpelican_bar';
-?>
+			<?php
+				$db_host = '127.0.0.1';
+				$db_user = 'kingpelican_mgmt';
+				$db_password = 'kingpelican2016';
+				$db_name = 'kingpelican_bar';
+			?>
 
 			<?php
 				$db_connect = new mysqli($db_host, $db_user, $db_password, $db_name);
@@ -39,33 +38,37 @@
 					$db_connect->set_charset("utf8");
 				}
 			?>
-<!-- For test comment out
+
 			<table>
-For test comment out ここでループの実験中。続きは明日・・・。 -->
+				<tr>
+					<th>NO</th>
+					<th>BREWERY</th>
+					<th>BEERNAME</th>
+					<th>Locality</th>
+					<th>Style</th>
+					<th>ABV</th>
+				</tr>
+
 			<?php
 				$menu_sql = 'SELECT * FROM beer_menu;';
+				$index_no = 0;
 				if ($sql_result = $db_connect->query($menu_sql)) {
-					$menu_index = $sql_result->fetch_array();
-				}
-				foreach ($menu_index as $menu_no){
-					print $menu_no;
-				}
-				$result->close();
-
-				unset($menu_no);
+					while ($menu_info = $sql_result->fetch_assoc()){
+						print "<tr>";
+							print "<td>{$menu_info['no']}</td>";
+							print "<td>{$menu_info['brewery1']}</td>";
+							print "<td>{$menu_info['brewery2']}</td>";
+							print "<td>{$menu_info['beername1']}</td>";
+							print "<td>{$menu_info['beername2']}</td>";
+							print "<td>{$menu_info['locality']}</td>";
+							print "<td>{$menu_info['style1']}</td>";
+							print "<td>{$menu_info['style2']}</td>";
+							print "<td>{$menu_info['abv']}</td>";
+						print "</tr>";
+					};
+				};
 			?>
-<!-- For test comment out
-				<tr>
-					<th>
-					</th>
-				</tr>
-
-				<tr>
-					<td>
-					</td>
-				</tr>
 			</table>
-For test comment out -->
 		</div>
 	</body>
 </html>
